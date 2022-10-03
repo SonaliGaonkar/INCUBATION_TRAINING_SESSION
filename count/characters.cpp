ea472c9fc81a22513e9fcf07612acb326a93cf9a
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-10-03 17:28:39 +0530

    commit

--- count/characters.cpp
+++ count/characters.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
         char ch, filename[50];
-        int i = 0, count = 0;
+        int i = 0, count = 0, tot_word = 0;
+        string temp;
         ifstream file;
         cout << "Enter filename : ";
         cin >> filename;
@@ -23,5 +24,15 @@ int main()
         }
 
         std::cout << "\nNo.of characters in a file = " << count << "\n";
+        while (file >> temp)
+        {
+                if (temp == "\n" || temp == "\r" || temp == "\t")
+                        continue;
+                tot_word++;
+        }
+        file.close();
+        cout << endl
+             << "Total Words = " << tot_word;
+        cout << endl;
         return 0;
 }
