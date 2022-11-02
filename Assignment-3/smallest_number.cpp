edfaf55663bb1238b6117078ab6cef46961af223
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-11-02 19:01:59 +0530

    commit

---
+++ Assignment-3/smallest_number.cpp
@@ -0,0 +1,28 @@
+// Write a program to accept a list of N integers. Accept integer K. Find the Kth smallest number in the list and its position.
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int kthSmallest(int arr[], int N, int K)
+{
+    sort(arr, arr + N);
+    return arr[K - 1];
+}
+int main()
+{
+    int N, K, arr[100];
+    std::cout << "Enter no of integers : ";
+    std::cin >> N;
+    std::cout << "Enter values in list : ";
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    std::cout << "Enter integer K : ";
+    std::cin >> K;
+
+    cout << "K'th smallest element is "
+         << kthSmallest(arr, N, K);
+    return 0;
+}
