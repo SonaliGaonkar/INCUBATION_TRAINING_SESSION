ea5ceb93633583448acc3fb43a4c2c8e50a36267
SonaliGaonkar <sonali.gaonkar@cctech.co.in>
2022-11-03 18:05:35 +0530

    commit

--- Assignment-3/circle.cpp
+++ Assignment-3/circle.cpp
@@ -2,88 +2,76 @@
 // Select appropriate data structure and explain why you chose it.
 // Arrange the circles in increasing order of their area and print all the data of each circle.
 
-/* Reason to use vector data structure : I have used vector as a data structure
-data can be easily accessed using vector
-I have also used class to store multiple data and used vector to store multiple objects
-*/
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
-class Cordinates
-{
-public:
-    int x, y;
-};
+#define MAX 100
+
 class Circle
 {
 public:
     int radius;
-    float area = 0;
-    vector<Cordinates> cordinate;
-};
-class Data
-{
+    int x;
+    int y;
+    float area;
+
 public:
-    vector<Circle> circles;
+    void getDetails(void)
+    {
+        cout << "\nEnter radius: ";
+        cin >> radius;
+        cout << "Enter co-ordinate x: ";
+        cin >> x;
+        cout << "Enter co-ordinate y: ";
+        cin >> y;
 
-    void getData(int n)
+        area = 3.14 * radius * radius;
+    };
+    void putDetails(void)
     {
-        Circle circle;
-        Cordinates cordinates;
-        for (int i = 0; i < n; i++)
-        {
-            cout << "Enter the Radius of Circle : " << i + 1 << endl;
-            int radius;
-            cin >> radius;
-            circle.radius = radius;
-            cout << "Enter the X and Y coordinate of Circle : " << i + 1 << endl;
-            int x, y;
-            cin >> x >> y;
-            cordinates.x = x;
-            cordinates.y = y;
-            circle.cordinate.emplace_back(cordinates);
-            circles.emplace_back(circle);
-            circle.cordinate.clear();
-        }
-    }
-    void calculateArea()
-    {
-        for (int i = 0; i < circles.size(); i++)
-        {
-            circles[i].area = 3.14 * circles[i].radius * circles[i].radius;
-        }
-    }
-    void printData()
-    {
-        cout << "\n";
-        sort(circles.begin(), circles.end(), [](Circle const &a, Circle const &b)
-             { return a.area < b.area; });
-        for (int i = 0; i < circles.size(); i++)
-        {
-            cout << "Circle " << i + 1 << " : " << endl;
-            cout << "\t"
-                 << "Radius - " << circles[i].radius << endl;
-            cout << "\t"
-                 << "Cordinate X - " << circles[i].cordinate[0].x << endl;
-            cout << "\t"
-                 << "Cordinate Y - " << circles[i].cordinate[0].y << endl;
-
-            cout << "\t"
-                 << "Area - " << circles[i].area << endl;
-        }
+        cout << "\nradius:" << radius;
+        cout << "\nco-ordinate x:" << x;
+        cout << "\nco-ordinate y:" << y;
+        cout << "\nArea:" << area;
     }
 };
 
 int main()
 {
-    Data circleData;
-    int n;
-    cout << "Enter the number of circles" << endl;
+    class Circle s[MAX]; // array of objects creation
+    int n, i, j, temp;
+
+    cout << "Enter total number of circles: ";
     cin >> n;
-    circleData.getData(n);
-    circleData.calculateArea();
-    circleData.printData();
-    return EXIT_SUCCESS;
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter data for circle " << i + 1;
+        s[i].getDetails();
+        std::cout << "\n";
+    }
+
+    cout << endl;
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "\nData of circle " << (i + 1);
+        for (i = 0; i < n; i++)
+        {
+            for (j = i + 1; j < n; j++)
+            {
+                if (s[i].area > s[j].area)
+                {
+                    temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+            }
+            s[i].putDetails();
+        }
+
+        std::cout << "\n";
+    }
+
+    return 0;
 }
